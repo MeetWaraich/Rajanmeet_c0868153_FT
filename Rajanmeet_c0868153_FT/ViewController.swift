0000e6c2138fb258999e5926f4b266ebd500da81
MeetWaraich <rwaraich599@gmail.com>
2022-11-07 18:35:33 -0500

    Initial Commit

---
+++ Rajanmeet_c0868153_FT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rajanmeet_c0868153_FT
+//
+//  Created by Richa Dua on 07/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
