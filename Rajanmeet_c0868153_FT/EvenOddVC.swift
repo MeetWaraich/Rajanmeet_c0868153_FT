2c58448bbd81efb70f21bee80b9bd93da28d07dd
MeetWaraich <rwaraich599@gmail.com>
2022-11-07 21:49:21 -0500

    Final commit

--- Rajanmeet_c0868153_FT/EvenOddVC.swift
+++ Rajanmeet_c0868153_FT/EvenOddVC.swift
@@ -14,6 +14,8 @@ class EvenOddVC: UIViewController {
     @IBOutlet weak var OddButton: UIButton!
     
     @IBOutlet weak var number: UILabel!
+
+    var score : Int! = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -39,14 +41,25 @@ class EvenOddVC: UIViewController {
         let number = Int(self.number.text!) ?? 0
         if isEven(number: number){
             
-            print("Even")
+            score += 1
+            let infoAlert = UIAlertController(title: "Result:", message: "Correct answer", preferredStyle: .alert)
+            
+            infoAlert.addAction(UIAlertAction(title: "Play again!", style: .default, handler: nil))
+
+            infoAlert.addAction(UIAlertAction(title: "Show progress", style: .default, handler: {_ in self.ShowProgressVC()}))
+
+            self.present(infoAlert, animated: true)
+            loadRandomNumber()
+
         }
         else {
             let alertController = UIAlertController(
-                   title: "Your title", message: "Result: Oopss... Wrong answer Right Answer is Even ", preferredStyle: .alert)
+                   title: "Result:", message: "Oopss... Wrong answer Right Answer is Even ", preferredStyle: .alert)
             let defaultAction = UIAlertAction(
-                   title: "Close Alert", style: .default, handler: nil)
-            //you can add custom actions as well
+                   title: "Play Again!", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            let defaultAction2 = UIAlertAction(
+                   title: "Show Progress", style: .default, handler: nil)
             alertController.addAction(defaultAction)
 
             present(alertController, animated: true, completion: nil)
@@ -57,12 +70,23 @@ class EvenOddVC: UIViewController {
     @objc func checkOdd() {
         let number = Int(self.number.text!) ?? 0
         if !isEven(number: number){
-            print("Odd")
+            loadRandomNumber()
+            score += 1
+            let infoAlert = UIAlertController(title: "Result:", message: "Correct answer", preferredStyle: .alert)
+            
+            infoAlert.addAction(UIAlertAction(title: "Play again!", style: .default, handler: nil))
+
+            infoAlert.addAction(UIAlertAction(title: "Show progress", style: .default, handler: {_ in self.ShowProgressVC()}))
+            loadRandomNumber()
+            self.present(infoAlert, animated: true)
         }
         else {
             let alertController = UIAlertController(
-                   title: "Play Again!", message: "Result: Oopss... Wrong answer Right Answer is Odd ", preferredStyle: .alert)
+                   title: "Result:", message: "Oopss... Wrong answer Right Answer is Odd ", preferredStyle: .alert)
             let defaultAction = UIAlertAction(
+                   title: "Play Again!", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            let defaultAction2 = UIAlertAction(
                    title: "Show Progress", style: .default, handler: nil)
             alertController.addAction(defaultAction)
 
@@ -71,9 +95,24 @@ class EvenOddVC: UIViewController {
 
         }
     }
+   
+    
+    func ShowProgressVC(){
+
+        print("Hello")
+
+            let mainSB : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+
+            let homeVC = mainSB.instantiateViewController(withIdentifier: "ShowTVScene")
+
+            navigationController?.pushViewController(homeVC, animated: true)
+
+        }
+
     
     func restartGame() {
-  
+            score = 0
+        
     }
     
     
---
+++ Rajanmeet_c0868153_FT/ShowProgressVC.swift
@@ -0,0 +1,31 @@
+//
+//  ShowProgressVC.swift
+//  Rajanmeet_c0868153_FT
+//
+//  Created by Richa Dua on 07/11/22.
+//
+
+import UIKit
+
+class ShowProgressVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
